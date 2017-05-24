f71acedfe5648244e99a97b6ea2fed481aa347df
Zack Bristor <zbristor@gmail.com>
2017-05-24 13:25:27 -0400

    first commit of first project

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	/* this is a multi-line comment
+	 * Author: Zack
+	 * Date: 5/24/17
+	 * This us our first program
+	 */
+	public static void main(String[] args) {
+		// This is an in line comment
+		System.out.println("Hello, World!");
+
+	}
+
+}
